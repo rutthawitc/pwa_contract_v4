c25e1685b998ca6876a1a1cedb3693dc521e7330
rutthawit ch <rutthawit@gmail.com>
2023-09-29 09:06:45 +0700

    --fix Thai currency

--- libs/FormatCurrency.js
+++ libs/FormatCurrency.js
@@ -4,17 +4,17 @@ const FormatCurrency = (number) => {
   //   style: 'currency',
   //   currency: 'THB',
   // });
-
+  console.log(number);
   // Check if the number is not null or undefined
   if (number !== null && number !== undefined) {
     // Use toLocaleString to format the number with commas and two decimal places
     const formattedNumber = number.toLocaleString('th-TH', {
-      style: 'currency',
-      currency: 'THB',
+      minimumFractionDigits: 2,
     });
 
     // Replace the currency symbol with ' บาท'
-    const formattedCurrency = formattedNumber.replace('THB', 'บาท');
+    //const formattedCurrency = formattedNumber.replace('THB', 'บาท');
+    const formattedCurrency = `${formattedNumber} บาท`;
 
     return formattedCurrency;
     // If the number is null or undefined, return an empty string or handle it as needed
